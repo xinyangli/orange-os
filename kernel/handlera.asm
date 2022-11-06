3f19d469a763b6597e96d3c964c738ee592e0dc8
Qing_LKYi <597572246@qq.com>
2022-11-06 23:49:40 +0800

    upd

--- kernel/handlera.asm
+++ kernel/handlera.asm
@@ -1,6 +1,35 @@
 [SECTION .text]
 
 extern PtDisp
+extern	p_proc_ready
+extern	tss
+extern	k_reenter
+
+; struct PROCESS
+P_STACKBASE	equ	0
+GSREG		equ	P_STACKBASE
+FSREG		equ	GSREG		+ 4
+ESREG		equ	FSREG		+ 4
+DSREG		equ	ESREG		+ 4
+EDIREG		equ	DSREG		+ 4
+ESIREG		equ	EDIREG		+ 4
+EBPREG		equ	ESIREG		+ 4
+KERNELESPREG	equ	EBPREG		+ 4
+EBXREG		equ	KERNELESPREG	+ 4
+EDXREG		equ	EBXREG		+ 4
+ECXREG		equ	EDXREG		+ 4
+EAXREG		equ	ECXREG		+ 4
+RETADR		equ	EAXREG		+ 4
+EIPREG		equ	RETADR		+ 4
+CSREG		equ	EIPREG		+ 4
+EFLAGSREG	equ	CSREG		+ 4
+ESPREG		equ	EFLAGSREG	+ 4
+SSREG		equ	ESPREG		+ 4
+P_STACKTOP	equ	SSREG		+ 4
+P_LDT_SEL	equ	P_STACKTOP
+P_LDT		equ	P_LDT_SEL	+ 4
+
+TSS3_S_SP0	equ	4
 
 CharPos equ ((80 * 4 + 10) * 2)
 
